d5e1340b62161cd24654ef39204033884f3d3027
NathanLoPresto <nlopresto14@gmail.com>
2022-09-01 15:18:39 -0500

    Trimmed Fat, finished comments

--- python/tests/Orserstimtest.py
+++ python/tests/Orserstimtest.py
@@ -7,6 +7,10 @@ Lucas Koerner, koerner.lucas@stthomas.edu
 Nathan LoPresto, lopr5624@stthomas.edu
 """
 
+#TODO
+#To run this script, config.yaml has to be properly set up
+#Follow pyripherals directions for correct implementation
+
 #Read "install" section in covg_fpga for pyripherals install directions
 from pyripherals.peripherals.DDR3 import DDR3
 from pyripherals.core import FPGA, Endpoint
@@ -88,33 +92,29 @@ daq.DAC[0].filter_select(operation="clear")
 for x in range(20):
     daq.DAC[0].write(int(0x11223344))
 
+#Not needed most likely, system will defualt to host mode during host-driven writes
 daq.DAC[0].set_data_mux("host")
 
-#These were needed for old DDR write
-#TODO: Addd relevant spi commands into command block
-clamp_num =0
-cmd_ch = clamp_num *2 +1
-cc_ch = clamp_num *2
+#TODO: Add relevant spi commands into command block
 dac_offset = 0x1e00
-offset_adjust_k = 1 + 2.1/3
-sequence_data = 5 * 1e-3
-cmd_voltage = sequence_data * offset_adjust_k * 10
 
 #Instance variable of ddr, set with cmd and cc signals
 cmd_signal = 212
 cc_signal = np.ones(4194304, dtype=np.uint16) * dac_offset
 
 #Signals loaded into daq.ddr object 
-daq.ddr.data_arrays[cmd_ch] = cmd_signal
-daq.ddr.data_arrays[cc_ch] = cc_signal
+daq.ddr.data_arrays[1] = cmd_signal
+daq.ddr.data_arrays[0] = cc_signal
 
+#DDR is loaded with waveforms
 daq.ddr.write_setup()
 block_pipe_return, speed_MBs = daq.ddr.write_channels(set_ddr_read=False)
 
+#FIFOs reset
 daq.ddr.reset_mig_interface()
 daq.ddr.reset_fifo('ALL')
-# self.daq.ddr.write_finish()
-# DEBUG
+
+# FIFOs restarted, data mux set to read from DDR
 bits = [daq.ddr.endpoints['ADC_WRITE_ENABLE'].bit_index_low,
         daq.ddr.endpoints['DAC_READ_ENABLE'].bit_index_low]
 daq.ddr.fpga.set_ep_simultaneous(daq.ddr.endpoints['ADC_WRITE_ENABLE'].address, bits, [1, 1])
